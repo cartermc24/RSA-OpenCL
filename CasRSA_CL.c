8674e080060ded59eb984ca7c91eb9b53e7be68c
Carter McCardwell <carter@mccardwell.net>
2015-11-21 01:18:20 -0600

    Initial commit of the first working build
    Note this commit is a very early working version of the OpenCL version of RSA.  It is not well optimized yet and only processes encryption at the moment.
    
    Signed-off-by: Carter McCardwell <carter@mccardwell.net>

--- CasRSA_CL.c
+++ CasRSA_CL.c
@@ -2,23 +2,31 @@
 //  CasRSA_CL.c
 //  CasRSA_CL
 //
-//  Created by Carter McCardwell on 12/7/14.
-//  Licensed under the MIT license, attribution is required.
+//  Created by Carter McCardwell on 11/18/15.
+//  Copyright (c) 2015 Carter McCardwell. All rights reserved.
 //
 
 #define __NO_STD_VECTOR
 #define MAX_SOURCE_SIZE (0x100000)
-#ifndef NUMWORDS //May be manually defined at compile time
-#define NUMWORDS 1000
+#ifndef MAXDIGITS
+#define	MAXDIGITS	500		/* maximum length bignum */
 #endif
+
+#define PLUS		1		/* positive sign bit */
+#define MINUS		-1		/* negative sign bit */
 
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
-#include <math.h>
 
-#include <CL/cl.h>
+#include <OpenCL/OpenCL.h>
+
+typedef struct {
+    char digits[MAXDIGITS];         /* represent the number */
+    int signbit;			/* 1 if positive, -1 if negative */
+    int lastdigit;			/* index of high-order digit */
+} bignum;
 
 const unsigned long WordMask = 0x00000000FFFFFFFFUL;
 
@@ -29,58 +37,114 @@ char* stradd(const char* a, const char* b){
     return strcat(strcat(ret, a) ,b);
 } //This function adds two string pointers together
 
-bool is_prime(int target)
+void print_bignum(bignum *n)
 {
-  for (int i = 2; i <= sqrt(target); i++)
-  {
-    if ((target % i) == 0)
-    {
-      return false;
+    int i;
+    
+    if (n->signbit == MINUS) printf("- ");
+    for (i=n->lastdigit; i>=0; i--)
+        printf("%c",'0'+ n->digits[i]);
+    
+    printf("\n");
+}
+
+void int_to_bignum(int s, bignum *n)
+{
+    int i;				/* counter */
+    int t;				/* int to work with */
+    
+    if (s >= 0) n->signbit = PLUS;
+    else n->signbit = MINUS;
+    
+    for (i=0; i<MAXDIGITS; i++) n->digits[i] = (char) 0;
+    
+    n->lastdigit = -1;
+    
+    t = abs(s);
+    
+    while (t > 0) {
+        n->lastdigit ++;
+        n->digits[ n->lastdigit ] = (t % 10);
+        t = t / 10;
     }
-  }
-  return true;
+    
+    if (s == 0) n->lastdigit = 0;
+}
+
+void initialize_bignum(bignum *n)
+{
+    int_to_bignum(0,n);
+}
+
+int scan_for_start_str(char* num_str)
+{
+    int places = MAXDIGITS;
+    for (int i = 0; i < MAXDIGITS; i++)
+    {
+        if (num_str[i] != 0) { places = i; }
+    }
+    return places;
+}
+
+void initialize_bignum_with_str(char* num_str, bignum *big_num)
+{
+    int_to_bignum(0, big_num);
+    for (int i = 0; i < MAXDIGITS; i++) { big_num->digits[i] = num_str[i]; }
+    big_num->lastdigit = scan_for_start_str(num_str);
 }
 
 int main(int argc, const char * argv[])
 {
-    printf("\nCasRSA_CL OpenCL 1.2 implementation of RSA\nCarter McCardwell, Northeastern University NUCAR - http://coe.neu.edu/~cmccardw - mccardwell.net");
+    printf("CasRSA_CL OpenCL 1.2 implementation of RSA\nCarter McCardwell, Northeastern University NUCAR\nhttp://coe.neu.edu/~cmccardw - mccardwell.net\n--------------------------------------\n");
 
+    if (argc != 3)
+    {
+        printf("Usage: ./CasRSA_CL conf_file outfile\nWhere the conf_file is a textfile that contains: [p] [q] [e] [message]\n");
+        return 1;
+    }
+    
     clock_t c_start, c_stop;
     c_start = clock(); //Create a clock to benchmark the time taken for execution
 
-    FILE *rsa_file;
+    char p_s[MAXDIGITS], q_s[MAXDIGITS], M_s[MAXDIGITS];
+    char ps[MAXDIGITS], qs[MAXDIGITS], Ms[MAXDIGITS];
+    bignum p, q, M, result;
+    int e;
+
+    FILE *conf_file;
     FILE *outfile;
     FILE *cl_code;
 
-    rsa_file = fopen(argv[1], "r");
-    if (onefile == NULL) { printf("\nerror_rsa_file\n"); return(1); }
+    conf_file = fopen(argv[1], "r"); //First number
+    if (conf_file == NULL) { printf("error_conf_file"); return(1); }
     outfile = fopen(argv[2], "w"); //The outfile, the result will be written here
-    if (outfile == NULL) { printf("\nerror (permission error: run with sudo or in directory the user owns)\n"); return(1); }
+    if (outfile == NULL) { printf("error (permission error: run with sudo or in directory the user owns)"); return (1); }
 
-    char n[NUMWORDS], totient[NUMWORDS], result[NUMWORDS];
-    char p[NUMWORDS], q[NUMWORDS], message[NUMWORDS];
-    int e;
-
-    fscanf(rsa_file, "%s %s %i %s", p, q, &e, message);
-    if (e < 1 || e > 3120 || !is_prime(e) || !(3120 % e == 0))
+    for (int i = 0; i < MAXDIGITS; i++)
     {
-        printf("\nRSA Fundamental Error: e must be 1<e<3120, a prime number, and not a devisor of 3120");
-        return(1);
+        p_s[i] = 0; q_s[i] = 0; M_s[i] = 0;
+        ps[i] = 0; qs[i] = 0; Ms[i] = 0;
     }
+    
+    int_to_bignum(0, &result);
 
-    int l_p = strlen(p);
-    int l_q = strlen(q);
-    int l_msg = strlen(message);
+    fscanf(conf_file, "%s %s %i %s", &ps, &qs, &e, &Ms);
+    
+	for (int i = 0; i < strlen(Ms); i++) { M_s[i] = Ms[strlen(Ms)-1-i] - '0'; }
+	for (int i = 0; i < strlen(ps); i++) { p_s[i] = ps[strlen(ps)-1-i] - '0'; }
+	for (int i = 0; i < strlen(qs); i++) { q_s[i] = qs[strlen(qs)-1-i] - '0'; }
 
-    printf("\nSize of p: %i, q: %i, message: %i", l_p, l_q, l_msg);
-
-    for (int i = 0; i < l_p; i++) { p[i] -= '0'; }
-    for (int i = 0; i < l_q; i++) { q[i] -= '0'; }
-    for (int i = 0; i < l_msg; i++) { message[i] -= '0'; }
-
-    char *cl_headers = "#pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable\n#define NUMWORDS ";
+	initialize_bignum_with_str(M_s, &M);
+	initialize_bignum_with_str(p_s, &p);
+	initialize_bignum_with_str(q_s, &q);
+    
+    printf("INPUT:\n\tM:"); print_bignum(&M);
+    printf("\n\tp:"); print_bignum(&p);
+    printf("\n\tq:"); print_bignum(&q);
+    
+    char *cl_headers = "#pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable\n#define PLUS 1\n#define MINUS -1\n#define MAXDIGITS ";
     char num[10];
-    sprintf(num, "%d", NUMWORDS);
+    sprintf(num, "%d", MAXDIGITS);
     char *append_str = stradd(cl_headers, num);
 
     cl_code = fopen("kernel_rsa.cl", "r");
@@ -107,11 +171,11 @@ int main(int argc, const char * argv[])
 
     cl_uint numberOfCores;
     clGetDeviceInfo(device, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(numberOfCores), &numberOfCores, NULL);
-    printf("\nRunning with %i compute units", numberOfCores); //Utilize the maximum number of compute units
+    printf("\nThis GPU supports %i compute units", numberOfCores); //Utilize the maximum number of compute units
 
     cl_uint maxThreads;
     clGetDeviceInfo(device, CL_KERNEL_WORK_GROUP_SIZE, sizeof(maxThreads), &maxThreads, NULL);
-    printf("\nRunning with %i threads per compute units", maxThreads); //Utilize the maximum number of threads/cu
+    //printf("\nRunning with %i threads per compute units", maxThreads); //Utilize the maximum number of threads/cu
 
     context = clCreateContext(0, 1, &device, NULL, NULL, &err);
     if (err != CL_SUCCESS) { printf("context"); }
@@ -122,7 +186,8 @@ int main(int argc, const char * argv[])
     cl_program program = clCreateProgramWithSource(context, 1, &append_str, &length, &err); //Compile program with expanded key included in the source
     if (err != CL_SUCCESS) { printf("createprogram"); }
 
-    err = clBuildProgram(program, 1, &device, "-I ./ -cl-std=CL1.2", NULL, NULL);
+    printf("\nBuilding CL Kernel...");
+    err = clBuildProgram(program, 1, &device, "-I ./ -cl-std=CL1.2", NULL, NULL); //The fourth parameter is specific to OpenCL 2.0
 
     if (err == CL_BUILD_PROGRAM_FAILURE) {
         printf("\nBuild Error = %i", err);
@@ -139,59 +204,67 @@ int main(int argc, const char * argv[])
         // Print the log
         printf("%s\n", log);
     }
+    printf("\t[Done]");
 
-    cl_mem cl_p, cl_q, cl_msg, cl_result;
+    cl_mem cl_p, cl_q, cl_M, cl_result;
     cl_int status = CL_SUCCESS;
     
-    cl_p = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(char)*NUMWORDS, &p, &status);
-    if (status != CL_SUCCESS || cl_num1 == NULL) { printf("\nCreate num1: %i", status); }
+    cl_p = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(bignum), &p, &status);
+    if (status != CL_SUCCESS || cl_p == NULL) { printf("\nCreate p: %i", status); }
 
-    cl_q = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(char)*NUMWORDS, &q, &status);
-    if (status != CL_SUCCESS || cl_num2 == NULL) { printf("\nCreate num2: %i", status); }
+    cl_q = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(bignum), &q, &status);
+    if (status != CL_SUCCESS || cl_q == NULL) { printf("\nCreate q: %i", status); }
 
-    cl_msg = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(char)*NUMWORDS, &message, &status);
-    if (status != CL_SUCCESS || cl_num2 == NULL) { printf("\nCreate message: %i", status); }
+    cl_M = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(bignum), &M, &status);
+    if (status != CL_SUCCESS || cl_M == NULL) { printf("\nCreate M: %i", status); }
 
-    cl_result = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(char)*NUMWORDS, &result, &status);
-    if (status != CL_SUCCESS || cl_result == NULL) { printf("\nCreate acc: %i", status); }
+    cl_result = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(bignum), &result, &status);
+    if (status != CL_SUCCESS || cl_result == NULL) { printf("\nCreate result: %i", status); }
 
-    cl_kernel rsa_kernel = clCreateKernel(program, "rsa_enc", &status);
+    cl_kernel rsa_kernel = clCreateKernel(program, "rsa_cypher", &status);
     if (status != CL_SUCCESS) { printf("\nclCreateKernel: %i", status); }
 
-    status = clSetKernelArg(rsa_kernel, 0, sizeof(cl_mem), &cl_num1);
-    status = clSetKernelArg(rsa_kernel, 1, sizeof(cl_mem), &cl_num2);
-    status = clSetKernelArg(rsa_kernel, 2, sizeof(cl_mem), &cl_msg);
+    status = clSetKernelArg(rsa_kernel, 0, sizeof(cl_mem), &cl_p);
+    status = clSetKernelArg(rsa_kernel, 1, sizeof(cl_mem), &cl_q);
+    status = clSetKernelArg(rsa_kernel, 2, sizeof(cl_mem), &cl_M);
     status = clSetKernelArg(rsa_kernel, 3, sizeof(cl_mem), &cl_result);
-    status = clSetKernelArg(rsa_kernel, 4, sizeof(int), &l_p);
-    status = clSetKernelArg(rsa_kernel, 5, sizeof(int), &l_q);
-    status = clSetKernelArg(rsa_kernel, 6, sizeof(int), &l_msg);
+    status = clSetKernelArg(rsa_kernel, 4, sizeof(int), &e);
     if (status != CL_SUCCESS) { printf("\nclSetKernelArg: %i", status); }
 
     size_t local_ws = 1, global_ws = 1;
-    printf("\nLocal: %d - Global: %d", local_ws, global_ws);
+    printf("\nRun Parameters: Local: %zu - Global: %zu", local_ws, global_ws);
 
     status = clEnqueueNDRangeKernel(queue, rsa_kernel, 1, NULL, &global_ws, &local_ws, 0, NULL, NULL);
     if (status != CL_SUCCESS) { printf("\nclEnqueueNDRangeKernel: %i", status); }
 
     clFinish(queue);
 
-    status = clEnqueueReadBuffer(queue, cl_result, CL_TRUE, 0, sizeof(char)*NUMWORDS, &result, 0, NULL, NULL);
+    status = clEnqueueReadBuffer(queue, cl_result, CL_TRUE, 0, sizeof(bignum), &result, 0, NULL, NULL);
     if (status != CL_SUCCESS) { printf("\nclEnqueueReadBuffer: %i", status); }
+
+    printf("\nEncrypted Result: ");
+    print_bignum(&result);
 
     c_stop = clock();
     float diff = (((float)c_stop - (float)c_start) / CLOCKS_PER_SEC ) * 1000;
 
-    printf("\nResult:\n%s\n", result);
-
+    printf("\nWriting result to outfile...");
+    for (int i = result.lastdigit+1; i --> 0;)
+    {
+        fprintf(outfile, "%c", result.digits[i]+'0');
+    }
+    printf("\t[Done]");
+    
     printf("\nDone - Time taken: %f ms\n", diff);
-    clReleaseMemObject(cl_num1);
-    clReleaseMemObject(cl_num2);
+    clReleaseMemObject(cl_p);
+    clReleaseMemObject(cl_q);
+    clReleaseMemObject(cl_M);
     clReleaseMemObject(cl_result);
-    fclose(onefile);
-    fclose(twofile);
+    fclose(conf_file);
     fclose(outfile);
     free(source_str);
     clReleaseContext(context);
     clReleaseCommandQueue(queue);
     return 0;
 }
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CasRSA_CL
 RSA Implementation in OpenCL 1.2
+
+Usage: ./CasRSA_CL conf_file outfile
+Where the conf_file is a text file that contains the following values seperated by a space:
+[p] [q] [e] [Message]
